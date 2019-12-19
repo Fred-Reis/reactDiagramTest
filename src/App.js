3634adbdfb96192b12930a53a6956d0867b7b5d3
Usuario <usuario@MacBook-Air-de-AM4.local>
2019-12-19 14:42:55 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Container, Box, Box1, Box2, Box3, Box4, Border} from './styles'
+import {DiLinux} from 'react-icons/di'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Border>
+        <Box />
+        <Box1 style={{marginTop:18}}/>
+        <Box2 style={{marginTop:16}}/>
+        <Box3 style={{marginTop:18}}/>
+        <Box4 />
+      </Border>
+      <button style={{marginRight: 500, width: 80, background: 'none', border: 'none', display:'flex', flexDirection:'column'}}>
+        <DiLinux style={{paddingLeft: 25}}/>
+        <text style={{fontSize:10, marginTop: 5}}>Voce esta aqui</text>
+      </button>
+      <Border>
+        <Box1 style={{marginTop:6, background:'red'}}/>
+        <Box style={{background:'palevioletred'}}/>
+        <Box3 style={{marginTop:6, background:'#6df6'}}/>
+        <Box4 style={{background:'#66cc'}}/>
+        <Box2 style={{marginTop:4, background:'#581'}}/>
+      </Border>
+    </Container>
   );
 }
 
+
+
 export default App;
+
---
+++ src/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #cccc;
+  flex-direction: column;
+
+`;
+
+export const Border = styled.div`
+  display:flex;
+  flex-direction: row ;
+  margin:30px;
+`;
+
+export const Box = styled.button`
+  width: 100px;
+  height: 100px;
+  background: #7159c1;
+  
+  border: 5px solid #ff6;
+`;
+
+export const Box1 = styled.button`
+  width: 80px;
+  height: 80px;
+  background: #714;
+  
+  border: 5px solid #ff6;
+`;
+
+export const Box2 = styled.button`
+  width: 70px;
+  height: 70px;
+  background: #993;
+  
+  border: 5px solid #ff6;
+`;
+
+export const Box3 = styled.button`
+  width: 65px;
+  height: 65px;
+  background: #543;
+  
+  border: 5px solid #ff6;
+`;
+
+export const Box4 = styled.button`
+  width: 90px;
+  height: 90px;
+  background: #7ff;
+  
+  border: 5px solid #ff6;
+`;
